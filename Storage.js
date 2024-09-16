6f8c6528e6e11b72cff142f8ac812281bc0dc713
actjacob <yakupaktar99@gmail.com>
2024-09-16 23:29:30 +0300

    localstorage add started

---
+++ Storage.js
@@ -0,0 +1,13 @@
+import { View } from "react"
+
+const Storage(){
+  return(
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+
+
+export default Storage
--- components/Background.js
+++ components/Background.js
@@ -1,34 +1,34 @@
-import React from "react";
-import { ImageBackground, View, StyleSheet } from "react-native";
+// import React from "react";
+// import { ImageBackground, View, StyleSheet } from "react-native";
 
-const Background = ({ children }) => {
-  return (
-    <ImageBackground
-      source={require("../image/fairy_bulutumsu_1.jpg")} // Düzeltme: require() kullanarak dosya yolunu belirtin
-      style={styles.background} // Düzeltme: style'da StyleSheet.background yerine styles.background kullanın
-      imageStyle={styles.image} // Fotoğrafı sola hizalamak için eklendi
-    >
-      <View style={styles.content}>{children}</View>
-    </ImageBackground>
-  );
-};
+// const Background = ({ children }) => {
+//   return (
+//     <ImageBackground
+//       source={require("../image/fairy_bulutumsu_1.jpg")} // Düzeltme: require() kullanarak dosya yolunu belirtin
+//       style={styles.background} // Düzeltme: style'da StyleSheet.background yerine styles.background kullanın
+//       imageStyle={styles.image} // Fotoğrafı sola hizalamak için eklendi
+//     >
+//       <View style={styles.content}>{children}</View>
+//     </ImageBackground>
+//   );
+// };
 
-// Stil tanımlamaları
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: "cover", // İsteğe bağlı: Resmin nasıl ayarlanacağını belirtir
-  },
-  image: {
-    resizeMode: "cover", // Fotoğrafın tam ekrana yayılmasını sağlar
-    left: 0,
-    rigth: null,
-  },
-  content: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center", // Çocuk bileşenlerin hizalanması
-  },
-});
+// // Stil tanımlamaları
+// const styles = StyleSheet.create({
+//   background: {
+//     flex: 1,
+//     resizeMode: "cover", // İsteğe bağlı: Resmin nasıl ayarlanacağını belirtir
+//   },
+//   image: {
+//     resizeMode: "cover", // Fotoğrafın tam ekrana yayılmasını sağlar
+//     left: 0,
+//     rigth: null,
+//   },
+//   content: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center", // Çocuk bileşenlerin hizalanması
+//   },
+// });
 
-export default Background; // Varsayılan olarak bileşeni dışa aktarın
+// export default Background; // Varsayılan olarak bileşeni dışa aktarın
